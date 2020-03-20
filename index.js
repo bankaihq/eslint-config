c3721932ef4fca2fa6ad69f796a00d3a338e24fd
Sanne Peters <sanne@bankai.eu>
2020-03-20 10:09:12 +0100

    extend rules of of eslint-config-standard
    Based on the ones coming in from @vue/standard as used in most Bankai projects

---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = {
+  extends: [
+    'eslint-config-standard',
+  ],
+  rules: {
+    'semi': [
+      'error',
+      'never',
+    ],
+    'indent': [
+      'error',
+      2,
+    ],
+    'comma-dangle': [
+      'error',
+      {
+        'arrays': 'always',
+        'objects': 'always',
+        'imports': 'never',
+        'exports': 'never',
+        'functions': 'ignore',
+      },
+    ],
+    'quotes': [
+      'error',
+      'single',
+    ],
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+    'no-console': [
+      'error',
+      {
+        'allow': [
+          'warn',
+          'error',
+        ],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        'js': 'never',
+      },
+    ],
+  },
+}
